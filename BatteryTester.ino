1f12039d8966f7beccb464b72ab2320e7e596d9e
TheDanger <github@fallenreich.com>
2015-05-28 21:28:16 +1000

    Added LCD support and code remarks

--- BatteryTester.ino
+++ BatteryTester.ino
@@ -1,25 +1,38 @@
-// Very simple Arduino Lithium-ion battery capacity tester
-// from electronicsblog.net
+/*
+ Very simple Arduino Lithium-ion battery capacity tester
+ from electronicsblog.net
+todo:  add safety circuit
+       add selectable voltage? need?
+       flashing LED to show finished state
 
+*/
+#include <LiquidCrystal.h>
 #define LED 13
-#define resistor 25
 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+
+// Change the values directly below to suit your discharge resistor
+int resistor = 25; // a 10 ohm resistor would be optimal
+int maxWatts = 25; // if you exceed this value you will fry your resistor   //Need to add code and circuit to break circuit if this is exceeded
+// Thats it!
+
+int watts = 0;
 float capacity = 0, value, voltage, current, time = 0;
-int maxWatts = 25;
 
 void measure (void) {
   value = analogRead(0);
   voltage = value / 1024 * 5.0;
   current = voltage / resistor;
   capacity = capacity + current / 3600;
-  watts = voltage * current
-  time++;
+  watts = voltage * current;
+          time++;
   Serial.print("Voltage= ");
   Serial.print(voltage);
   Serial.print("V Current= ");
   Serial.print(current);
   Serial.print("A Capacity= ");
-  Serial.print(capacity/1000);
+  Serial.print(capacity / 1000);
   Serial.print("mAh ");
   Serial.print(" Watts= ");
   Serial.print(watts);
@@ -27,6 +40,19 @@ void measure (void) {
   Serial.print("Discharging time= ");
   Serial.print(time / 60);
   Serial.println("m ");
+
+  lcd.setCursor(0, 1);
+  lcd.print("V: ");
+  lcd.print(voltage);
+  lcd.print(" I: ");
+  lcd.print(current);
+  lcd.print(" W: ");
+  lcd.print(watts);
+
+  lcd.setCursor(0, 2);
+  lcd.print("Time: ");
+  lcd.print(time / 60);
+  lcd.print(" Minutes");
 }
 boolean x = false;
 ISR(TIMER1_OVF_vect) {
@@ -36,6 +62,7 @@ ISR(TIMER1_OVF_vect) {
   measure();
 }
 void setup() {
+  lcd.begin(16, 2);
   pinMode(LED, OUTPUT);
 
   TIMSK1 = 0x01; // enabled global and timer overflow interrupt;
