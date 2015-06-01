d98ac4f5a807f38bd78c4126a83648ce77bb46c4
TheDanger <github@fallenreich.com>
2015-06-02 02:47:52 +1000

    Many changes
    Ramping up project for home manufacture

--- BatteryTester.ino
+++ BatteryTester.ino
@@ -9,24 +9,153 @@ todo:  add safety circuit
 
 */
 #include <LiquidCrystal.h>
+#include <Time.h>
 #define LED 13
-
+const int cutoffPin = 9;
+const int ratePin = 8;
+const int chargePin = 7;
+const int startPin = 10;
 //LiquidCrystal lcd(12, 11, 6, 5, 4, 3);
 LiquidCrystal lcd(12, 11, 3, 4, 5, 6);
 
 // Change the values directly below to suit your discharge resistor
-int resistor = 25; // a 10 ohm resistor would be optimal
-int maxWatts = 25; // if you exceed this value you will fry your resistor   //Need to add code and circuit to break circuit if this is exceeded
+int slowResistor = 15; // a 15 ohm resistor would be optimal
+int fastResistor = 5; //faster discharge of battery. More heat and possible battery shutout on smaller capacities. Use slow discharge for those.
+int slowMaxWatts = 25; // if you exceed this value you will fry your resistor   //Need to add code and circuit to break circuit if this is exceeded
+int fastMaxWatts = 50;
 float powerV = 4.34; // If readings are off check arduino supply voltage and adjust here
 // Thats it!
 
+float voltageCutoff = 2.00; // if the LiPo battery does not have internal cutoff security this will stop the test before damaging battery.
 int capacitymAh = 0;
 float capacity = 0, value, voltage, current, time = 0, watts = 0;
+int buttonState = 0;
+int lastButtonState = LOW;
+int startState = 0;
+int lastStartState = 1;
+int processSecond = second();
 
-void measure (void) {
+long debounceDelay = 100;    // the debounce time; increase if the output flickers
+
+boolean debounce(int pin) {
+  boolean state;
+  boolean previousState;
+
+  previousState = digitalRead(pin);
+  for (int counter = 0; counter < debounceDelay; counter++) {
+    delay(1);
+    state = digitalRead(pin);
+    if (state != previousState) {
+      counter = 0;
+      previousState = state;
+    }
+  }
+  return state;
+}
+
+
+
+boolean x = false;
+ISR(TIMER1_OVF_vect) {
+  TCNT1 = 0x0BDC;
+  x = !x;
+  Serial.print("ratePin2= ");
+  Serial.println(digitalRead(ratePin));
+  if (ratePin == 0) {
+    slowmeasure();
+  }
+  else if (ratePin == 1) {
+    fastmeasure();
+  }
+}
+
+void setup() {
+  lcd.begin(16, 2);
+  pinMode(LED, OUTPUT);
+  pinMode(cutoffPin, OUTPUT);
+  pinMode(ratePin, INPUT);
+  pinMode(chargePin, INPUT);
+  pinMode(startPin, INPUT);
+  digitalWrite(cutoffPin, LOW);
+  digitalWrite(LED, LOW);
+
+  //RTX Time Sync
+
+  if (timeStatus() != timeSet)
+    Serial.println("Unable to sync with the RTC");
+  else
+    Serial.println("RTC has set the system time");
+  digitalClockDisplay();
+  TIMSK1 = 0x01; // enabled global and timer overflow interrupt;
+  TCCR1A = 0x00; // normal operation page 148 (mode0);
+  TCNT1 = 0x0BDC; // set initial value to remove time error (16bit counter register)
+  TCCR1B = 0x04; // start timer/ set clock
+  Serial.begin(9600);
+};
+void loop () {
+  if (voltage == 0.00) { //flash LED when test complete (lithium battery under voltage cutoff)
+    //digitalWrite(LED, x);
+  }
+
+  if ((voltage <= voltageCutoff) && (voltage > 0.01)) {
+    digitalWrite(cutoffPin, LOW);               //*****Finish this
+    Serial.println("Voltage Cut Off Reached!");
+
+  }
+  if (ratePin == LOW) {
+    if  (watts >= slowMaxWatts) {
+      digitalWrite(cutoffPin, LOW);
+      Serial.println("Watt limit reached on SLOW discharge!");
+    }
+
+    else if (ratePin == HIGH) {
+      if  (watts >= fastMaxWatts) {
+        digitalWrite(cutoffPin, LOW);
+        Serial.println("Watt limit reached on FAST discharge!");
+      }
+    }
+  }
+
+  // buttonState = digitalRead(startPin);
+  // int reading = digitalRead(startPin);
+  startState = (debounce(startPin));
+  //Serial.print("Start state: ");
+  //Serial.println(startState);
+  // if (startState == HIGH && lastStartState == LOW) {
+  if (startState == HIGH) {
+
+    // lastStartState = startState;
+
+    if (buttonState == HIGH) {
+      digitalWrite(LED, HIGH);
+      Serial.println("Go!");
+      digitalWrite(cutoffPin, HIGH);
+      buttonState = LOW;
+    } else {
+      digitalWrite(LED, LOW);
+      digitalWrite(cutoffPin, LOW);
+      Serial.println("No Go!");
+      buttonState = HIGH;
+    }
+  }
+
+  if (processSecond != second()) {
+    processSecond = second();
+    value = analogRead(0);
+    voltage = (value / 1024) * powerV;
+    current = voltage / slowResistor;
+    capacity = capacity + current / 3600;
+    watts = voltage * current;
+    capacitymAh = capacity * 1000;
+    lcdupdate();
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////
+void fastmeasure (void) {
   value = analogRead(0);
   voltage = (value / 1024) * powerV;
-  current = voltage / resistor;
+  current = voltage / fastResistor;
   capacity = capacity + current / 3600;
   watts = voltage * current;
   capacitymAh = capacity * 1000;
@@ -36,8 +165,8 @@ void measure (void) {
   Serial.print("V Current= ");
   Serial.print(current);
   Serial.print("A Capacity= ");
-   Serial.print(capacity);
-     Serial.print(" ");
+  Serial.print(capacity);
+  Serial.print(" ");
   Serial.print(capacitymAh);
   Serial.print("mAh ");
   Serial.print(" Watts= ");
@@ -48,7 +177,7 @@ void measure (void) {
   Serial.println("m ");
 
   lcd.setCursor(0, 0);
- // lcd.print("V:");
+  // lcd.print("V:");
   lcd.print(voltage);
   lcd.print("V ");
   lcd.print(current);
@@ -62,30 +191,85 @@ void measure (void) {
   lcd.print("M ");
   lcd.print("C:");
   lcd.print(capacitymAh);
-    lcd.print("mAh");
-  
+  lcd.print("mAh");
 }
-boolean x = false;
-ISR(TIMER1_OVF_vect) {
-  TCNT1 = 0x0BDC;
-  x = !x;
-
-  measure();
+void lcdupdate (void) {
+  lcd.setCursor(0, 0);
+  lcd.print(voltage);
+  lcd.print("V ");
+  lcd.print(current);
+  lcd.print("A ");
+  lcd.print(watts);
+  lcd.print("W");
+  lcd.setCursor(0, 1);
+  lcd.print("T:");
+  lcd.print(time / 60);
+  lcd.print("M ");
+  lcd.print("C:");
+  lcd.print(capacitymAh);
+  lcd.print("mAh");
 }
-void setup() {
-  lcd.begin(16, 2);
-  pinMode(LED, OUTPUT);
 
-  TIMSK1 = 0x01; // enabled global and timer overflow interrupt;
-  TCCR1A = 0x00; // normal operation page 148 (mode0);
-  TCNT1 = 0x0BDC; // set initial value to remove time error (16bit counter register)
-  TCCR1B = 0x04; // start timer/ set clock
-  Serial.begin(9600);
-};
-void loop () {
-  if (voltage==0.00) { //flash LED when test complete (lithium battery under voltage cutoff)  
-      digitalWrite(LED, x);
-  }
+void slowmeasure (void) {
+  value = analogRead(0);
+  voltage = (value / 1024) * powerV;
+  current = voltage / slowResistor;
+  capacity = capacity + current / 3600;
+  watts = voltage * current;
+  capacitymAh = capacity * 1000;
+  time++;
+  Serial.print("Voltage= ");
+  Serial.print(voltage);
+  Serial.print("V Current= ");
+  Serial.print(current);
+  Serial.print("A Capacity= ");
+  Serial.print(capacity);
+  Serial.print(" ");
+  Serial.print(capacitymAh);
+  Serial.print("mAh ");
+  Serial.print(" Watts= ");
+  Serial.print(watts);
+  Serial.print("W ");
+  Serial.print("Discharging time= ");
+  Serial.print(time / 60);
+  Serial.println("m ");
 
-};
+  lcd.setCursor(0, 0);
+  // lcd.print("V:");
+  lcd.print(voltage);
+  lcd.print("V ");
+  lcd.print(current);
+  lcd.print("A ");
+  lcd.print(watts);
+  lcd.print("W");
 
+  lcd.setCursor(0, 1);
+  lcd.print("T:");
+  lcd.print(time / 60);
+  lcd.print("M ");
+  lcd.print("C:");
+  lcd.print(capacitymAh);
+  lcd.print("mAh");
+}
+
+void digitalClockDisplay() {
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year());
+  Serial.println();
+}
+
+void printDigits(int digits) {
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if (digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
